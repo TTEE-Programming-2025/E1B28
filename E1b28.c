a1a9f5cfb3ac2c85f82f5354f15144c643c80998
E1b28 <sunny040295@gmail.com>
2025-04-02 11:34:25 +0800

    寫了步驟一的個人風個畫面

--- E1b28.c
+++ E1b28.c
@@ -1,10 +1,35 @@
-#include <stdio.h> 
-#include <stdlib.h>
-int main(void)
-{
-	int input,sum=0,max=0,passcount=0,fail=0;
-	int scores[5];
-	int correctpassword=178;
-	printf("請輸入密碼:");
-	scanf("%d",&input);
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main() {
+    /******************** 步驟1：顯示歡迎畫面 ********************/
+    printf("\n\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    printf("*程式設計作業1*\n");
+    printf("\n");
+    printf("班級：E1b\n");
+    printf("座號：28\n");
+    printf("姓名：陳可晴\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("請按任意鍵繼續... \n");
+    printf("* *\n");
+    printf("**************************************************\n");
+    
+    system("PAUSE");
+    system("CLS");
+
+    
+    return 0;
 }
