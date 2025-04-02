38989aad77ffd3a3169b736399ed024662d8b5d9
E1b28 <sunny040295@gmail.com>
2025-04-02 10:31:29 +0800

    int名稱和printf

--- E1b28.c
+++ E1b28.c
@@ -1,1 +1,10 @@
 #include <stdio.h> 
+#include <stdlib.h>
+int main(void)
+{
+	int input,sum=0,max=0,passcount=0,fail=0;
+	int scores[5];
+	int correctpassword=178;
+	printf("½Ð¿é¤J±K½X:");
+	scanf("%d",&input);
+}
