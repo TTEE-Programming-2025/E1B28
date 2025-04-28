b8862e4d4f2b5c76e7f3660204f555dec81b41be
E1b28 <sunny040295@gmail.com>
2025-04-28 21:10:59 +0800

    打a b c不同做法

--- E1b28.c
+++ E1b28.c
@@ -1,3 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
+
+void displayWelcomeScreen();
+int checkPassword();
+void displayMainMenu();
+void drawTriangle();
+void showMultiplicationTable();
+void clearScreen();
+
+int main() {
+ 
+    if (!checkPassword()) {
+        return 0;  
+    }
+    
+
+    while (1) {
+        displayMainMenu();
+        char choice;
+        printf("請輸入您的選擇: ");
+        scanf(" %c", &choice);
+        
+        switch (choice) {
+            case 'a':
+            case 'A':
+                drawTriangle();
+                break;
+            case 'b':
+            case 'B':
+                showMultiplicationTable();
+                break;
+            case 'c':
+            case 'C':
+                while (1) {
+                    printf("Continue? (y/n): ");
+                    char continueChoice;
+                    scanf(" %c", &continueChoice);
+                    
+                    if (continueChoice == 'n' || continueChoice == 'N') {
+                        printf("程式結束，感謝使用！\n");
+                        return 0;
+                    } else if (continueChoice == 'y' || continueChoice == 'Y') {
+                        clearScreen();
+                        break;  
+                    } else {
+                        printf("錯誤輸入，請重新輸入！\n");
+                    }
+                }
+                break;
+            default:
+                printf("無效的選擇，請重新輸入！\n");
+                system("pause");
+                clearScreen();
+        }
+    }
+    
+    return 0;
+}
