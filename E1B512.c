8cfba2c5c487924c2b5479512c74496e9be4bffc
E1b28 <sunny040295@gmail.com>
2025-05-12 21:36:28 +0800

    密碼設定

--- E1B512.c
+++ E1B512.c
@@ -26,3 +26,40 @@ void confirmArrangement(int r[], int c[], int count);
 void chooseByUser();
 int isValidFormat(const char *s, int *r, int *c);
 void exitProgram();
+int main() {
+    srand(time(NULL));
+    generateRandomSeats();
+
+    printWelcome();
+    if (!checkPassword()) {
+        printf("密碼錯誤超過三次，程式結束。\n");
+        return 0;
+    }
+
+    while (1) {
+        showMenu();
+        printf("請輸入選項 (a/b/c/d): ");
+        fgets(input, sizeof(input), stdin);
+        char choice = tolower(input[0]);
+
+        switch (choice) {
+            case 'a':
+                showSeats();
+                waitForEnter();
+                break;
+            case 'b':
+                arrangeSeats();
+                break;
+            case 'c':
+                chooseByUser();
+                break;
+            case 'd':
+                exitProgram();
+                break;
+            default:
+                printf("輸入錯誤，請重新選擇。\n");
+        }
+    }
+
+    return 0;
+}
