0b0ae175b355046744dda7e31b1bb03d123e8ea2
E1b28 <sunny040295@gmail.com>
2025-05-27 00:16:41 +0800

    清除畫面及設置密碼

--- E1B28526.c
+++ E1B28526.c
@@ -14,4 +14,27 @@ typedef struct {
 
 Student students[MAX];
 int student_count = 0;
+void clear_screen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
 
+int login() {
+    int input, tries = 0;
+    while (tries < 3) {
+        clear_screen();
+        printf("請輸入4位數密碼：");
+        scanf("%d", &input);
+        if (input == PASSWORD) return 1;
+        else {
+            printf("密碼錯誤，請再試一次。\n");
+            tries++;
+            getchar(); getchar(); 
+        }
+    }
+    printf("錯誤次數過多，程式結束。\n");
+    return 0;
+}
